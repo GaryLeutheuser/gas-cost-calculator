1f8cca9782c94c28da1c0642f6b056276ca7722e
Gary Leutheuser <g.leutheuser@gmail.com>
2021-04-10 20:44:37 -0600

    Initial commit

---
+++ popup.js
@@ -0,0 +1,28 @@
+let changeColor = document.getElementById("changeColor");
+
+chrome.storage.sync.get("color", ({ color }) => {
+    changeColor.style.backgroundColor = color;
+});
+
+changeColor.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: displayCost,
+    });
+});
+
+function displayCost() {
+    let selection = window.getSelection().toString();
+    let number = parseInt(selection);
+    let costPerMile = (1.0 / number) * 3.00;
+    let costPerYear = costPerMile * 12000;
+    alert(number + " MPG = $" + costPerMile.toFixed(2) + " per mile.\nThis is an annual cost of $" + costPerYear.toFixed(2));
+}
+
+function setPageBackgroundColor() {
+    chrome.storage.sync.get("color", ({ color }) => {
+        document.body.style.backgroundColor = color;
+    });
+}
